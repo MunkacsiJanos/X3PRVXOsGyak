3474d32210ad218cb7e40073241b114bae3e84b3
MunkacsiJanos <78854513+MunkacsiJanos@users.noreply.github.com>
2021-03-10 18:02:14 +0100

    Create parent.c

---
+++ X3PRVX_0310/X3PRVXgyak3/parent.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+    char *args[]={"./child",NULL};
+    
+	int child_pid, wait_pid, status;
+	child_pid=fork();
+    
+	if(child_pid==0)
+   	{
+   		execvp(args[0],args); 
+		printf("ERROR\n");
+                exit(1);
+	}
+		else
+    		{
+			wait_pid=wait(NULL);
+			printf("%d %d",getpid(),wait_pid);
+		}
+}
